f77f62d42ad4d519895b240e466d9e7139183a14
Kyle Schiller <kyle.schiller@udacity.com>
2017-01-30 11:13:24 -0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import todosList from '../imports/components/todosList/todosList';
-
-angular.module('shadi-snacks', [
-  angularMeteor,
-  todosList.name,
-]);
